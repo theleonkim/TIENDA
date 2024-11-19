de1da6102eb9e00c5044e230b74913651781686e
Kimberly Leon <Kimberly Leon@LAPTOP-9OPLIQPF.lan>
2024-11-19 11:37:16 -0600

    Proyecto tienda

---
+++ src/main/java/com/Tienda/service/ProductoService.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Tienda.service;
+
+/**
+ *
+ * @author Kimberly Leon
+ */
+import com.Tienda.domain.Producto;
+import java.util.List;
+
+
+public interface ProductoService {
+    
+    // Se obtiene un listado de productos en un List
+    public List<Producto> getProductos(boolean activos);
+    
+   // Se obtiene un Producto, a partir del id de un producto
+    public Producto getProducto(Producto producto);
+    
+    // Se inserta un nuevo producto si el id del producto esta vacío
+    // Se actualiza un producto si el id del producto NO esta vacío
+    public void save(Producto producto);
+    
+    // Se elimina el producto que tiene el id pasado por parámetro
+    public void delete(Producto producto);
+    
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+    
+    //Lista de productos utilizando consultas con JPQL    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup);
+    
+     //Lista de productos utilizando consultas con SQL Nativo
+    public List<Producto> metodoNativo(double precioInf, double precioSup);
+    
+    public List<Producto> busarPorNombre(String nombre);
+    
+    public List<Producto> busarPorExistencia(Integer existencias);
+}
---
+++ src/main/java/com/Tienda/service/impl/ProductoServiceImpl.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Tienda.service.impl;
+
+/**
+ *
+ * @author Kimberly Leon
+ */
+
+import com.Tienda.domain.Producto;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.Tienda.dao.ProductoDao;
+import com.Tienda.service.ProductoService;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+    @Autowired
+    private ProductoDao productoDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> getProductos(boolean activos) {
+        var lista = productoDao.findAll();
+
+        if (activos) {
+            //Remueve de la lista los elementos donde
+            //el atributo activo es false
+            lista.removeIf(e -> !e.isActivo());
+
+        }
+        return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Producto getProducto(Producto producto) {
+        return productoDao.findById(producto.getIdProducto()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Producto producto) {
+        productoDao.save(producto);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Producto producto) {
+        productoDao.delete(producto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup) {
+        return productoDao.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
+    }
+    
+    @Override
+    @Transactional(readOnly=true)    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup) {
+        return productoDao.metodoJPQL(precioInf, precioSup);
+    }
+    
+    @Override
+    @Transactional(readOnly=true)    
+    public List<Producto> metodoNativo(double precioInf, double precioSup) {
+        return productoDao.metodoNativo(precioInf, precioSup);
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public List<Producto> busarPorNombre(String nombre) {
+        return productoDao.findByDescripcionContainingOrderByPrecio(nombre);
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public List<Producto> busarPorExistencia(Integer existencias) {
+        return productoDao.findByExistencias(existencias);
+    }
+    
+}
